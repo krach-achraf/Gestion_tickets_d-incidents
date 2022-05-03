c52f99f1085b29d462652ac74a87fb7b7e2e4d6c
krach-achraf <krachachraf98@gmail.com>
2022-05-03 22:05:32 +0000

    initialise project

---
+++ src/main/java/spring/devoir/gestion_tickets/GestionTicketsApplication.java
@@ -0,0 +1,13 @@
+package spring.devoir.gestion_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/devoir/gestion_tickets/GestionTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.devoir.gestion_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
